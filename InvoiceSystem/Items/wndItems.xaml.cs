2d2a2c48ac08390a935a5b119480c9ffe8fa03b3
jstewart4u2c <121924540+jstewart4u2c@users.noreply.github.com>
2023-11-16 13:33:42 -0700

    Added SQL and began db link

--- InvoiceSystem/Items/wndItems.xaml.cs
+++ InvoiceSystem/Items/wndItems.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using InvoiceSystem.Main;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,17 +23,92 @@ namespace InvoiceSystem.Items
     /// </summary>
     public partial class wndItems : Window
     {
+        #region Attributes
+        /// <summary>
+        /// Boolean variable to be accessed by main to check if something has been changed
+        /// </summary>
+        public bool ItemModified;
+        #endregion
+
+        /// <summary>
+        /// Creates new database object 
+        /// </summary>
+        clsDataAccess db;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <exception cref="Exception"></exception>
         public wndItems()
         {
-            InitializeComponent();
+            try
+            {
+                InitializeComponent();
+
+                //Initialize Database
+                db = new clsDataAccess();
+
+                DataSet ds;
+
+                int ReturnValues = 0;
+                
+                //Execute SQL to select all from db
+                ds = db.ExecuteSQLStatement("SELECT * FROM ItemDesc", ref ReturnValues);
+
+                ds.Tables[0].TableName = "ItemDesc";
+
+                //loop to grab all data and output to itemList
+                for(int i=0; i < ds.Tables[0].Rows.Count; i++)
+                {
+                    itemList.Items.Add(ds.Tables[0].Rows[i][1].ToString() + " " + ds.Tables[0].Rows[i].ItemArray[2].ToString());
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
         }
 
+        /// <summary>
+        /// Item Object containing variables for item info
+        /// </summary>
         public class Item
         {
             //Private Variables
             private int code;
             private double cost;
             private string description;
+        }
+
+        /// <summary>
+        /// when Main Menu is clicked, hide current window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <exception cref="Exception"></exception>
+        private void MainMenu_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                this.Hide();                 
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        private void SearchMenu_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                this.Hide();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
         }
     }
 }
